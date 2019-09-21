d4421aaff2935f06223f738b1923caa9bd77d4a8
Sebastian Roldan <sebasoldanm@gmail.com>
2019-09-21 16:01:53 -0500

    Frente Estrategico - Model, Controller, Permission

---
+++ app/Models/Autoevaluacion/FrenteEstrategico.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models\Autoevaluacion;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+class FrenteEstrategico extends Model
+{
+    //
+    use LogsActivity;
+    protected static $logUnguarded = true;
+    /**
+     * Conexión usada por el modelo.
+     *
+     * @var string
+     */
+    protected $connection = 'autoevaluacion';
+
+    /**
+     * Tabla asociada con el modelo.
+     *
+     * @var string
+     */
+    protected $table = 'TBL_Frentes_Estrategicos';
+
+    /**
+     * LLave primaria del modelo.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'PK_FES_Id';
+
+    /**
+     * Atributos del modelo que no pueden ser asignados en masa.
+     *
+     * @var array
+     */
+    protected $guarded = ['PK_FES_Id', 'created_at', 'updated_at'];
+
+}
