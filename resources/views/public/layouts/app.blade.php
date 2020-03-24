1f5d117e06d4bdffcdef4a1bb62f7c7c0887db03
Sebastian Roldan <sebasroldanm@gmail.com>
2020-03-24 09:33:46 -0500

    Cambio titulo

--- resources/views/public/layouts/app.blade.php
+++ resources/views/public/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Sistema de Informacion para la Autoevaluación V3.0</title>
+    <title>Sistema de Informacion para la Autoevaluación V4.0</title>
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="assets/images/favicons/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
